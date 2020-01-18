c9995d78c256dc84140351086bc2104fd7f158fd
AlexanderRagnarsson <alexanderrag19@ru.is>
2020-01-18 21:15:33 +0000

    Pictures

--- tesseracter.py
+++ tesseracter.py
@@ -1,9 +1,11 @@
 import pytesseract
 
+# image = 'tesseract_test.jfif'
+image = '2020-01-18 (3).png'
 # tesseract_test.jfif as a_image
 
-print(dir(pytesseract))
+# print(dir(pytesseract))
 
-print(pytesseract.image_to_string('tesseract_test.jfif'))
-
+print(pytesseract.image_to_string(image))
+# print(pytesseract.image_to_pdf_or_hocr(image))
 print('Vei')
