dc0840c2dfde1227faf0f28de292a56b3f8ce92a
AlexanderRagnarsson <alexanderrag19@ru.is>
2020-01-20 01:19:36 +0000

    Working image finder (with uncertainty)

--- tesseracter.py
+++ tesseracter.py
@@ -43,31 +43,49 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-img = cv.imread('Hearthstone myndir\\Elven archer full.png',0)
+img = cv.imread('Hearthstone myndir\\Depth charge full.png',0)
 img2 = img.copy()
-template = cv.imread('Hearthstone myndir\\New folder\\1 mana (abusive) bigger.png',0)
+template = cv.imread('Hearthstone myndir\\New folder\\1 hp (abusive) bigger.png',0)
 
+total = 0
+total_list = []
 
 w, h = template.shape[::-1]
 # All the 6 methods for comparison in a list
-methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-            'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+# methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
+#             'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
+# Hin þrjú skila mjög stóru max_val
+methods = ['cv.TM_CCOEFF_NORMED','cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF_NORMED']
+
 for meth in methods:
     img = img2.copy()
     method = eval(meth)
     # Apply template Matching
     res = cv.matchTemplate(img,template,method)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    total += max_val
+    total_list.append(max_val)
+
+    # Þessi kóði sýnir hvar hann spottaði hlutinn sem hann fann (voða kúl)
     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img,top_left, bottom_right, 255, 2)
-    plt.subplot(121),plt.imshow(res,cmap = 'gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122),plt.imshow(img,cmap = 'gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle(meth)
-    plt.show()
+    # if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
+    #     top_left = min_loc
+    # else:
+    #     top_left = max_loc
+    # bottom_right = (top_left[0] + w, top_left[1] + h)
+    # cv.rectangle(img,top_left, bottom_right, 255, 2)
+    # plt.subplot(121),plt.imshow(res,cmap = 'gray')
+    # plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+    # plt.subplot(122),plt.imshow(img,cmap = 'gray')
+    # plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
+    # plt.suptitle(meth)
+    # plt.show()
+
+
+# Eftir að ákveða fastana
+if 0.9 < total/3 < 1.1:
+    print('Init')
+else:
+    print('Notinit') 
+
+print(total_list)
