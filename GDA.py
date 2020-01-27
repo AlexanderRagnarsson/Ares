2f82af2404d8d2d206e84e889adb798d5b55a1dc
Fridjon-hinn-fyrst <gvendurfe19@ru.is>
2020-01-27 01:06:28 +0000

    Veit ekki hverju ég breytti eða afvherju.

--- GDA.py
+++ GDA.py
@@ -17,10 +17,15 @@ class GDA:
         gray = GDA.__delta_image(gray_image)
         gray = gray.convert("RGBA")
         gray_array = numpy.array(gray)
+        for xy in GDA.TEST+GDA.labb:
+            gray_array[xy[1]][xy[0]]=(255,0,0,255)
+
+
+
         i2 = Image.fromarray(gray_array)
         i2.show()
         i2.convert("RGBA").save('test.PNG', 'PNG')
-
+    TEST = []
     def __get_loops_from_image(image):
         loop_images = []
         gray = image.convert('L')
@@ -30,10 +35,15 @@ class GDA:
         while x != len(gray_array[0])-1:
             if GDA.__delta_pixel((x, y), gray_array) > GDA.SEPARATION_CONSTANT:
                 position = (x, y)
-                is_loop, loop_image, highest_x_value_in_loop = GDA.__find_delta_loop(gray_array, position)
+                is_loop = GDA.__find_delta_loop(gray_array, position)
                 if is_loop:
-                    GDA.__extract_loop((x, y), gray_array)
+                    GDA.TEST += [position]
+                    loop_image, biggest_x_value_for_cord_y = GDA.__extract_loop((x, y), gray_array)
+                    loop_images += [loop_image]
+                    x = biggest_x_value_for_cord_y
             x += 1
+        for image1 in loop_images:
+            image1.show()
 
     def __extract_loop(position, gray_array):
         smallest_x = position[0]
@@ -72,27 +82,22 @@ class GDA:
         image_loop_array = []
         for i in range(dy):
             image_loop_array += [[0]*dx]
-
+        biggest_x_value_for_cord_y = position[0]
         for xy in loop:
             x, y = xy
+
+            if y == position[1]:
+                if x > biggest_x_value_for_cord_y:
+                    biggest_x_value_for_cord_y = x
+
             image_loop_array[y-smallest_y][x-smallest_x] = 255
 
         image_loop = Image.fromarray(numpy.array(image_loop_array))
-        image_loop.show()
-
-
-
-
-
-
-
-
-
-
+        return image_loop, biggest_x_value_for_cord_y
 
     def __find_delta_loop(gray_array, position):
-        # if loop is not found return False, None, None
-        # if loop is found return True, Image_of_loop, highest_x_value_in_loop relative too grid
+        # if loop is not found return False
+        # if loop is found return True
         x, y = position
         x1, x2 = x, x
         # finding edge of border
@@ -118,28 +123,21 @@ class GDA:
             walk_mem[y][i] = 1
             walk_mem[y + 1][i] = 1
             walk_mem[y + 2][i] = 0
-        print(position, "SEARCH---------------EDGE:",x1,x2)
         if GDA.__find_the_zero((x, y), walk_mem, gray_array):
-            return True, None, None
-        return False, None, None
-
-    TEST = []
+            return True
+        return False
+    labb = []
     def __find_the_zero(position, walk_mem, grey_array):
-        GDA.TEST += [position]
+        GDA.labb += [position]
         x, y = position
         walk_mem[y][x] = 1
         for dxy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
             if walk_mem[y+dxy[1]][x+dxy[0]] == 0:
-                print("found 0!!!!!!!!!!!!", x, y)
                 return True
             elif walk_mem[y+dxy[1]][x+dxy[0]] == -1 and GDA.__delta_pixel((x+dxy[0], y+dxy[1]), grey_array) > GDA.SEPARATION_CONSTANT:
                 if GDA.__find_the_zero((x + dxy[0], y + dxy[1]), walk_mem, grey_array) == True:
                     return True
         return False
-
-
-
-
 
     def __delta_pixel(cord, gray_image_array):
         x, y = cord
@@ -149,11 +147,6 @@ class GDA:
         for dxy in dxy_matrix:
             delta_sum += abs(current_value - gray_image_array[y + dxy[0]][x + dxy[1]])
         return delta_sum//8
-
-
-
-
-
 
     def __delta_image(grascale_image):
         pixels_original = numpy.array(grascale_image)
@@ -183,7 +176,7 @@ class GDA:
         delta_image = Image.fromarray(numpy.array(pixels_delta))
         return delta_image
 
-image = Image.open("Hearthstone_numbers/number_10_2.png")
+image = Image.open("Hearthstone_numbers/number_12_2.png")
 t1 = time.time()
 GDA.get_client(image)
 t2 = time.time()
